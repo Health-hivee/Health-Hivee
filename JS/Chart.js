62681837ba44f2be4034289eeefc672220b1e20f
Your Name <you@example.com>
2023-04-26 15:02:15 +0100

    add

--- JS/Chart.js
+++ JS/Chart.js
@@ -1,5 +1,5 @@
-const xValues = [50, 60, 70, 80, 90, 100, 110,];
-const yValues = [7, 8, 8, 9, 9, 9, 10, 11,];
+const xValues = [50, 60, 70, 80, 90, 100, 110];
+const yValues = [7, 8, 8, 9, 9, 9, 10, 11];
 
 new Chart("myChart", {
   type: "line",
@@ -19,8 +19,36 @@ new Chart("myChart", {
     legend: { display: false },
     scales: {
       yAxes: [{ ticks: { min: 6, max: 16 } }],
-      
+    },
+  },
+});
 
+let xValue = ["Read", "New"];
+
+let yValue = [60, 40];
+
+let barColors = ["#574EBB"];
+
+new Chart("myChart1", {
+  type: "doughnut",
+
+  data: {
+    labels: xValue,
+
+    datasets: [
+      {
+        backgroundColor: barColors,
+
+        data: yValue,
+      },
+    ],
+  },
+
+  options: {
+    title: {
+      display: true,
+
+      text: "No of tips read",
     },
   },
 });
